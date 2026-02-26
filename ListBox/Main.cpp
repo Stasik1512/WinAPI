47213feab7f1976583f8752cfb472a82006f9015
Nekit K. <14nikita@list.ru>
2026-02-26 16:09:04 +0500

    Homework DONE 2/3

--- ListBox/Main.cpp
+++ ListBox/Main.cpp
@@ -1,72 +1,166 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <cstdio>
-#include"resource.h"
+#include "resource.h"
 
 using namespace std;
 
+CONST CHAR* VALUES[] = { "THIS", "is", "my", "first", "List", "Box" };
 
-CONST CHAR* VALUES[] = { "THIS", "is", "my","first", "List", "Box" };
+BOOL CALLBACK AddDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); // Обьявление дочернего окна
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); // ОБбьявление гл окна
 
-BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdSnow)
 {
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
 	return 0;
 }
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_INITDIALOG:
-	{
-		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
-		HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
-		for (int i = 0; i < sizeof(VALUES) / sizeof(VALUES[0]); i++)
+		case WM_INITDIALOG:
 		{
-			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)VALUES[i]);
-		}
+			HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
-	}
-	break;
+			HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+			for (int i = 0; i < sizeof(VALUES) / sizeof(VALUES[0]); i++)
+			{
+				SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)VALUES[i]);
+			}
+		}
+	break;  
+
 	case WM_COMMAND:
+	{
 		switch (LOWORD(wParam))
 		{
-		case IDOK:
+			case IDC_BUTTON_ADD:
+			{
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), // Открываем дочернее окно
+					hwnd,
+					AddDialogProc, (LPARAM)hwnd);  // указатель на главное окно
+			}
+			break;
+			case IDC_BUTTON_DELET:
+			{
+				HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+
+				// индекс выбранного элемента
+				INT iSelected = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+
+				if (iSelected != LB_ERR)
+				{
+					CONST INT SIZE = 256;
+					CHAR sz_buffer[SIZE] = {};
+					SendMessage(hListBox, LB_GETTEXT, iSelected, (LPARAM)sz_buffer);
+
+					SendMessage(hListBox, LB_DELETESTRING, iSelected, 0);
+				}
+				else
+				{
+					MessageBox(hwnd, "Сначала выберите элемент для удаления",
+						"info", MB_OK | MB_ICONINFORMATION);
+				}
+			}
+			break;
+			case IDOK:
+			{
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+
+				INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+				SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
+				CHAR sz_message[SIZE] = {};
+				sprintf(sz_message, "You have choose #%i  with value '%s'", i + 1, sz_buffer);
+				if (i != LB_ERR)
+					MessageBox(hwnd, sz_message, "info", MB_OK | MB_ICONINFORMATION);
+				else
+					MessageBox(hwnd, "First, select the item", "info", MB_OK | MB_ICONINFORMATION);
+			}
+			break;
+
+			case IDCANCEL:
+			{
+				EndDialog(hwnd, 0);
+			}
+			break;
+		}
+	}
+		break;
+
+		case WM_CLOSE:
 		{
-			/*HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
-			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-
-			const char* messages[] = {
-				"You have selected 1 item - Box",
-				"You have selected 2 item - Combo",
-				"You have selected 3 item - first",
-				"You have selected 4 item - my",
-				"You have selected 5 item - is",
-				"You have selected 6 item - This"
-			};
-
-			MessageBox(hwnd, messages[i], "Информация", MB_OK | MB_ICONINFORMATION);*/
-
-			CONST INT SIZE = 256; 
-			CHAR sz_buffer[SIZE] = {};
-			HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
-
-			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0); // LB_ LIST BOX GETCURSEL Get current selection
-			SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
-			CHAR sz_message[SIZE] = {};
-			sprintf(sz_message, "You have choose #%i  with value '%s'", i + 1, sz_buffer);
-			if(i != LB_ERR)MessageBox(hwnd, sz_message, "info", MB_OK | MB_ICONINFORMATION);
-			else MessageBox(hwnd, "First, select the item", "info", MB_OK | MB_ICONINFORMATION);
+			EndDialog(hwnd, 0);
 		}
 		break;
-		case IDCANCEL: EndDialog(hwnd, 0);
-		}
-		break;
-	case WM_CLOSE:
-		EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
+
+// Дочернее окно 
+BOOL CALLBACK AddDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND hParentWnd;  // указатель на главное окно
+
+	switch (uMsg)
+	{
+		case WM_INITDIALOG:
+		{
+			hParentWnd = (HWND)lParam;
+
+			// иконка
+			HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+			// фокус
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+			SetFocus(hEdit);
+			return FALSE;
+		}
+		break;
+
+		case WM_COMMAND:
+		{
+			switch (LOWORD(wParam))
+			{
+				case IDOK:
+				{
+					// Получаем текст из поля
+					CONST INT SIZE = 256;
+					CHAR sz_text[SIZE] = {};
+					HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+					GetWindowText(hEdit, sz_text, SIZE);
+
+					// Проверка, что текст не пустой
+					if (strlen(sz_text) > 0)
+					{
+						// Добавляем текст в ListBox
+						HWND hListBox = GetDlgItem(hParentWnd, IDC_LIST);
+						SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_text);
+					}
+					EndDialog(hwnd, IDOK);
+				}
+				break;
+
+				case IDCANCEL:
+				{
+					EndDialog(hwnd, 0);
+				}
+				break;
+			}
+		}
+		break;
+
+		case WM_CLOSE:
+		{
+			EndDialog(hwnd, 0);
+		}
+		break;
+	}
+		return FALSE;
 }
