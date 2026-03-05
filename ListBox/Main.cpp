949ed4555820a2fd8b99360b7a4ff0d72b853b14
Nekit K. <14nikita@list.ru>
2026-03-05 14:50:59 +0500

    ListBox Catalog DONE

--- ListBox/Main.cpp
+++ ListBox/Main.cpp
@@ -1,216 +1,272 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <cstdio>
+#include <string>
 #include "resource.h"
 
 using namespace std;
 
-CONST CHAR* VALUES[] = { "THIS", "is", "my", "first", "List", "Box" };
-
-BOOL CALLBACK AddDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); // Обьявление дочернего окна
-BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); // ОБбьявление гл окна
-
+BOOL CALLBACK AddDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK EditDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+void LoadFileListToLB(HWND hListBox, LPCSTR szFolder = ".")  // текущий каталог
+{
+    // все файлы
+    char szSearchPath[MAX_PATH] = {};
+    GetCurrentDirectory(MAX_PATH, szSearchPath);
+    strcat(szSearchPath, "\\*.*");
+
+    WIN32_FIND_DATA FindData;
+    HANDLE hFind = FindFirstFile(szSearchPath, &FindData);
+
+    if (hFind != INVALID_HANDLE_VALUE)
+    {
+        do
+        {
+            // текущий и родительский каталоги
+            if (strcmp(FindData.cFileName, ".") != 0 &&
+                strcmp(FindData.cFileName, "..") != 0)
+            {
+                
+                if (!(FindData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
+                {
+                    SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)FindData.cFileName);
+                }
+            }
+        } while (FindNextFile(hFind, &FindData));
+
+        FindClose(hFind);
+    }
+}
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdSnow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
-	return 0;
+    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+    return 0;
 }
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg)
-	{
-	case WM_INITDIALOG:
-	{
-		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+        SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
-		HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
-		for (int i = 0; i < sizeof(VALUES) / sizeof(VALUES[0]); i++)
-		{
-			SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)VALUES[i]);
-		}
-	}
-	break;
+        HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
 
-	case WM_COMMAND:
-	{
-		switch (LOWORD(wParam))
-		{
-		case IDC_LIST:
-			if (HIWORD(wParam) == LBN_DBLCLK)
-				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, EditDialogProc, 0);
-			break;
-		case IDC_BUTTON_ADD:
-		{
-			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), // Открываем дочернее окно
-				hwnd,
-				AddDialogProc, (LPARAM)hwnd);  // указатель на главное окно
-		}
-		break;
-		case IDC_BUTTON_DELET:
-		{
-			HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+        // Очиcтка
+        SendMessage(hListBox, LB_RESETCONTENT, 0, 0);
 
-			// индекс выбранного элемента
-			INT iSelected = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+        // Загружаем список файлов
+        LoadFileListToLB(hListBox);
+    }
+    break;
 
-			if (iSelected != LB_ERR)
-			{
-				CONST INT SIZE = 256;
-				CHAR sz_buffer[SIZE] = {};
-				SendMessage(hListBox, LB_GETTEXT, iSelected, (LPARAM)sz_buffer);
+    case WM_COMMAND:
+    {
+        switch (LOWORD(wParam))
+        {
+        case IDC_LIST:
+            if (HIWORD(wParam) == LBN_DBLCLK)
+                DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, EditDialogProc, 0);
+            break;
 
-				SendMessage(hListBox, LB_DELETESTRING, iSelected, 0);
-			}
-			else
-			{
-				MessageBox(hwnd, "Сначала выберите элемент для удаления",
-					"info", MB_OK | MB_ICONINFORMATION);
-			}
-		}
-		break;
-		case IDOK:
-		{
-			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE] = {};
-			HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+        case IDC_BUTTON_ADD:
+        {
+            DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD),
+                hwnd, AddDialogProc, (LPARAM)hwnd);
+        }
+        break;
 
-			INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
-			SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
-			CHAR sz_message[SIZE] = {};
-			sprintf(sz_message, "You have choose #%i  with value '%s'", i + 1, sz_buffer);
-			if (i != LB_ERR)
-				MessageBox(hwnd, sz_message, "info", MB_OK | MB_ICONINFORMATION);
-			else
-				MessageBox(hwnd, "First, select the item", "info", MB_OK | MB_ICONINFORMATION);
-		}
-		break;
+        case IDC_BUTTON_DELET:
+        {
+            HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+            INT iSelected = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
 
-		case IDCANCEL:
-		{
-			EndDialog(hwnd, 0);
-		}
-		break;
-		}
-	}
-	break;
+            if (iSelected != LB_ERR)
+            {
+                // Спрашиваем подтверждение перед удалением
+                if (MessageBox(hwnd, "Удалить выбранный элемент?", "Подтверждение",
+                    MB_YESNO | MB_ICONQUESTION) == IDYES)
+                {
+                    SendMessage(hListBox, LB_DELETESTRING, iSelected, 0);
+                }
+            }
+            else
+            {
+                MessageBox(hwnd, "Сначала выберите элемент для удаления",
+                    "Информация", MB_OK | MB_ICONINFORMATION);
+            }
+        }
+        break;
 
-	case WM_CLOSE:
-	{
-		EndDialog(hwnd, 0);
-	}
-	break;
-	}
-	return FALSE;
+        case IDOK:
+        {
+            CONST INT SIZE = 256;
+            CHAR sz_buffer[SIZE] = {};
+            HWND hListBox = GetDlgItem(hwnd, IDC_LIST);
+
+            INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+            if (i != LB_ERR)
+            {
+                SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
+                CHAR sz_message[SIZE] = {};
+                sprintf(sz_message, "Вы выбрали файл: %s", sz_buffer);
+                MessageBox(hwnd, sz_message, "Информация", MB_OK | MB_ICONINFORMATION);
+            }
+            else
+            {
+                MessageBox(hwnd, "Сначала выберите файл", "Информация",
+                    MB_OK | MB_ICONINFORMATION);
+            }
+        }
+        break;
+
+        case IDCANCEL:
+        {
+            EndDialog(hwnd, 0);
+        }
+        break;
+        }
+    }
+    break;
+
+    case WM_CLOSE:
+    {
+        EndDialog(hwnd, 0);
+    }
+    break;
+    }
+    return FALSE;
 }
 
-// Дочернее окно 
+// окно для добавления
 BOOL CALLBACK AddDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	static HWND hParentWnd;  // указатель на главное окно
+    static HWND hParentWnd;
 
-	switch (uMsg)
-	{
-	case WM_INITDIALOG:
-	{
-		hParentWnd = (HWND)lParam;
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        hParentWnd = (HWND)lParam;
 
-		// иконка
-		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+        HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+        SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
 
-		// фокус
-		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
-		SetFocus(hEdit);
-		return FALSE;
-	}
-	break;
+        HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+        SetFocus(hEdit);
+        return FALSE;
+    }
+    break;
 
-	case WM_COMMAND:
-	{
-		switch (LOWORD(wParam))
-		{
-		case IDOK:
-		{
-			// Получаем текст из поля
-			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE] = {};
-			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
-			GetWindowText(hEdit, sz_buffer, SIZE);
+    case WM_COMMAND:
+    {
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+        {
+            CONST INT SIZE = 256;
+            CHAR sz_buffer[SIZE] = {};
+            HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+            GetWindowText(hEdit, sz_buffer, SIZE);
 
-			// Проверка, что текст не пустой
-			if (strlen(sz_buffer) > 0)
-			{
-				// Добавляем текст в ListBox
-				HWND hListBox = GetDlgItem(hParentWnd, IDC_LIST);
-				if (SendMessage(hListBox, LB_FINDSTRING, 0, (LPARAM)sz_buffer) == LB_ERR)
-					SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
-			}
-			EndDialog(hwnd, IDOK);
-		}
-		break;
+            if (strlen(sz_buffer) > 0)
+            {
+                HWND hListBox = GetDlgItem(hParentWnd, IDC_LIST);
+                if (SendMessage(hListBox, LB_FINDSTRING, 0, (LPARAM)sz_buffer) == LB_ERR)
+                    SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+                else
+                    MessageBox(hwnd, "Такой элемент уже существует", "Ошибка",
+                        MB_OK | MB_ICONWARNING);
+            }
+            EndDialog(hwnd, IDOK);
+        }
+        break;
 
-		case IDCANCEL:
-		{
-			EndDialog(hwnd, 0);
-		}
-		break;
-		}
-	}
-	break;
+        case IDCANCEL:
+        {
+            EndDialog(hwnd, 0);
+        }
+        break;
+        }
+    }
+    break;
 
-	case WM_CLOSE:
-	{
-		EndDialog(hwnd, 0);
-	}
-	break;
-	}
-	return FALSE;
+    case WM_CLOSE:
+    {
+        EndDialog(hwnd, 0);
+    }
+    break;
+    }
+    return FALSE;
 }
 
+// окно для редактирования
 BOOL CALLBACK EditDialogProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	CONST INT SIZE = 256;
-	CHAR sz_buffer[SIZE] = {};
-	HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
-	HWND hParent = GetParent(hwnd);
-	HWND hListBox = GetDlgItem(hParent, IDC_LIST);
-	switch (uMsg)
-	{
-	case WM_INITDIALOG:
-	{
-		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)"Изменить выбранный элемент");
+    CONST INT SIZE = 256;
+    CHAR sz_buffer[SIZE] = {};
+    HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+    HWND hParent = GetParent(hwnd);
+    HWND hListBox = GetDlgItem(hParent, IDC_LIST);
 
-		INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
-		SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
-		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
-		SendMessage(hEdit, EM_SETSEL, 0, -1);
-		SendMessage(hEdit, EM_SETSEL, -1, -1);
-	}
-	break;
-	case WM_COMMAND:
-		switch (LOWORD(wParam))
-		{
-		case IDOK:
-		{
-			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			if (strlen(sz_buffer) > 0 && SendMessage(hListBox, LB_FINDSTRING, 0, (LPARAM)sz_buffer) == LB_ERR)
-			{
-				INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
-				SendMessage(hListBox, LB_DELETESTRING, i, 0);
-				SendMessage(hListBox, LB_INSERTSTRING, i, (LPARAM)sz_buffer);
-			}
-		}
-		case IDCANCEL:EndDialog(hwnd, 0);
-		}
-		break;
-	case WM_CLOSE:
-		EndDialog(hwnd, 0);
-	}
-	return FALSE;
+    switch (uMsg)
+    {
+    case WM_INITDIALOG:
+    {
+        SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)"Изменить выбранный элемент");
+
+        INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+        if (i != LB_ERR)
+        {
+            SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
+            SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+            SetFocus(hEdit);
+            SendMessage(hEdit, EM_SETSEL, 0, -1);
+        }
+    }
+    break;
+
+    case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDOK:
+        {
+            SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+            if (strlen(sz_buffer) > 0)
+            {
+                INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+                if (i != LB_ERR)
+                {
+                    // Проверяем, нет ли уже такого элемента
+                    if (SendMessage(hListBox, LB_FINDSTRING, 0, (LPARAM)sz_buffer) == LB_ERR)
+                    {
+                        SendMessage(hListBox, LB_DELETESTRING, i, 0);
+                        SendMessage(hListBox, LB_INSERTSTRING, i, (LPARAM)sz_buffer);
+                    }
+                    else
+                    {
+                        MessageBox(hwnd, "Такой элемент уже существует", "Ошибка",
+                            MB_OK | MB_ICONWARNING);
+                    }
+                }
+            }
+            EndDialog(hwnd, 0);
+            break;
+        }
+        case IDCANCEL:
+            EndDialog(hwnd, 0);
+            break;
+        }
+        break;
+
+    case WM_CLOSE:
+        EndDialog(hwnd, 0);
+        break;
+    }
+    return FALSE;
 }
